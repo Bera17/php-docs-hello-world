aa47b66218649aa0b4f7824c5b558ecea80e2ccd
Bera17 <43541945+Bera17@users.noreply.github.com>
2020-12-31 12:17:56 +0100

    Update index.php

--- index.php
+++ index.php
@@ -4,10 +4,7 @@ echo "Il est l'heure de manger";
 echo "Ce soir, c'est soupe aux champignons";
 echo "Coucou";
 echo "Salut à vous";
-   echo "Le site des bonnes nouvelles à changé ma vie, depuis le soleil est rayonnant";
-
-   echo "<h1> VIVE LES CACAHUÈTES </h1>";
-
+echo "Le site des bonnes nouvelles à changé ma vie, depuis le soleil est rayonnant";
 echo "HAHA";
 echo "<h1><3</h1>";
 echo "belle journée aujourd'hui"
